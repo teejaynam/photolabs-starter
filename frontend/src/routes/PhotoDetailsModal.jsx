37b5a9d04a17519484163bbb1321852d8d215668
TJ <teejaynam@gmail.com>
2023-09-21 17:32:38 -0400

    add the tags to complete the layour of the modal and related photos

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/PhotoDetailsModal.scss'
+import '../styles/PhotoListItem.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
-import PhotoListItem from 'components/PhotoListItem';
+import PhotoFavButton from 'components/PhotoFavButton';
+import PhotoList from 'components/PhotoList';
 
 const PhotoDetailsModal = ( props ) => {
-  const {closeModal, photo, photos, onLikePhoto, onUnlikePhoto} = props;
+  const {closeModal, photo, photos, likedPhotos, onLikePhoto, onUnlikePhoto } = props;
   const selectedPhoto = photos.find((item) => item.id === photo);
+  const {similar_photos} = selectedPhoto;
+
 
   const handleClose = () => {
     closeModal();
@@ -17,20 +21,60 @@ const PhotoDetailsModal = ( props ) => {
       <button className="photo-details-modal__close-button" onClick={handleClose}>
         <img src={closeSymbol} alt="close symbol" />
       </button>
-      {selectedPhoto && (
-        <div className="photo-details-modal__image">
-          <PhotoListItem
-            id={selectedPhoto.id}
-            location={selectedPhoto.location}
-            imageSource={selectedPhoto.urls.regular}
-            username={selectedPhoto.user.username}
-            profile={selectedPhoto.user.profile}
-            isLiked={selectedPhoto.isLiked} 
-            onLike={onLikePhoto}
-            onUnlike={onUnlikePhoto}
-          />
-        </div>
-      )}
+        {selectedPhoto && (
+          <>
+            
+            <div className='photo-details-modal__top-bar'>
+              <div>
+                <PhotoFavButton
+                  isLiked={likedPhotos.includes(selectedPhoto.id)}
+                  onLike={() => onLikePhoto(selectedPhoto.id)}
+                  onUnlike={() => onUnlikePhoto(selectedPhoto.id)}
+                />
+                <img src={selectedPhoto.urls.regular} className="photo-details-modal__image"/>
+              </div>
+            </div>
+
+            <div className='photo-details-modal__top-bar'>
+            <div className="photo-list__user-details">
+
+              <img
+                src={selectedPhoto.user.profile}
+                className="photo-list__user-profile"
+              />
+              <div className="photo-list__user-info">
+                <div>
+                  <span className="photo-details-modal__photographer-details">{selectedPhoto.user.username}</span>
+                  </div>
+                  <div>
+                  <span className="photo-list__user-location photo-details-modal__photographer-details">
+                    {selectedPhoto.location.city}, {selectedPhoto.location.country}
+                   </span>
+                </div>
+              </div>
+            </div>
+            </div>
+
+          </>
+        )}
+      <div className='photo-details-modal__header photo-details-modal__top-bar'>Similar Photos</div>
+      
+      <div className='photo-details-modal__images photo-details-modal__top-bar'>
+        {similar_photos && (
+          <>
+            {Object.keys(similar_photos).map((key) => (
+              <PhotoList
+                key={key}
+                photos={[similar_photos[key]]}
+                likedPhotos={likedPhotos}
+                onLikePhoto={onLikePhoto}
+                onUnlikePhoto={onUnlikePhoto}
+              />
+            ))}
+          </>
+        )}
+      </div>
+
     </div>
   );
 };
