137d42c8dc71a46af11c47c5a131bad200eed31f
TJ <teejaynam@gmail.com>
2023-09-21 12:18:59 -0400

    add TopNavigation bar and PhotoList component

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
 
-const HomeRoute = () => {
+import PhotoList from 'components/PhotoList';
+import TopNavigationBar from 'components/TopNavigationBar';
+import photos from "../mocks/photos.js"
+
+const HomeRoute = ({openModal}) => {
+  const [likedPhotos, setLikedPhotos] = useState([]);
+
+  const isFavPhotoExist = likedPhotos.length > 0 ? true : false;
+
+  const handleLikePhoto = (photoId) => {
+    if (!likedPhotos.includes(photoId)) {
+      setLikedPhotos([...likedPhotos, photoId]);
+    }
+  };
+
+  const handleUnlikePhoto = (photoId) => {
+    const updatedLikedPhotos = likedPhotos.filter((id) => id !== photoId);
+    setLikedPhotos(updatedLikedPhotos);
+  };
+  
+
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar isFavPhotoExist={isFavPhotoExist}/>
+      <PhotoList
+        photos={photos}
+        likedPhotos={likedPhotos}
+        onLikePhoto={handleLikePhoto}
+        onUnlikePhoto={handleUnlikePhoto}
+        openModal={openModal}
+      />
     </div>
   );
 };
