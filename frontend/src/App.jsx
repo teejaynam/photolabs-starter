c851cb51ebedf58f3c488f401fdd2d14bf9beddb
TJ <teejaynam@gmail.com>
2023-09-21 12:11:41 -0400

    add Homeroute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,47 @@
-import React from 'react';
-
-import PhotoListItem from './components/PhotoListItem';
+import React, {useState} from 'react';
 import './App.scss';
+import HomeRoute from 'routes/HomeRoute';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import photos from 'mocks/photos';
 
-// Note: Rendering a single component to build components in isolation
+
 const App = () => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  const openModal = (photo) => {
+    setSelectedPhoto(photo);
+    setModalOpen(true);
+  }
+
+  const closeModal = () => {
+    setSelectedPhoto(null);
+    setModalOpen(false);
+  }
+
+  const handleLikePhoto = (photoId) => {
+    if (!likedPhotos.includes(photoId)) {
+      setLikedPhotos([...likedPhotos, photoId]);
+    }
+  };
+
+  const handleUnlikePhoto = (photoId) => {
+    const updatedLikedPhotos = likedPhotos.filter((id) => id !== photoId);
+    setLikedPhotos(updatedLikedPhotos);
+  };
+
   return (
     <div className="App">
-      <PhotoListItem/>
+      <HomeRoute openModal={openModal}/>
+      {isModalOpen && (
+        <PhotoDetailsModal 
+          closeModal={closeModal} 
+          photo={selectedPhoto}
+          photos={photos}
+          onLikePhoto={handleLikePhoto} 
+          onUnlikePhoto={handleUnlikePhoto} 
+        />
+      )}
     </div>
   );
 };
