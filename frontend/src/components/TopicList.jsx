95878322de1f5fc9f725da8c6394b4a6a05835fc
TJ <teejaynam@gmail.com>
2023-09-21 12:17:09 -0400

    remove sample data and add span tag that shows the topic titles

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,31 +1,20 @@
 import React from "react";
 
 import "../styles/TopicList.scss";
+import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
-
-const TopicList = () => {
+const TopicList = (props) => {
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+      {props.topics.map((topic) => (
+        <TopicListItem
+          key={topic.id}
+          id={topic.id}
+          slug={topic.slug}
+          title={topic.title}
+        />
+      ))}
     </div>
   );
 };
-
 export default TopicList;
