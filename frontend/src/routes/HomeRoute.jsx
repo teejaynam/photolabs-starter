d68b9af5f1d97b3d31a5b3dfbe5e0cb701f85b61
TJ <teejaynam@gmail.com>
2023-09-21 17:31:57 -0400

    remove liked photo states and functions to lift them to App.jsx

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
 
 import '../styles/HomeRoute.scss';
-
 import PhotoList from 'components/PhotoList';
 import TopNavigationBar from 'components/TopNavigationBar';
 import photos from "../mocks/photos.js"
 
-const HomeRoute = ({openModal}) => {
-  const [likedPhotos, setLikedPhotos] = useState([]);
+const HomeRoute = (props) => {
+  const {openModal, likedPhotos, onLikePhoto, onUnlikePhoto} = props;
 
   const isFavPhotoExist = likedPhotos.length > 0 ? true : false;
-
-  const handleLikePhoto = (photoId) => {
-    if (!likedPhotos.includes(photoId)) {
-      setLikedPhotos([...likedPhotos, photoId]);
-    }
-  };
-
-  const handleUnlikePhoto = (photoId) => {
-    const updatedLikedPhotos = likedPhotos.filter((id) => id !== photoId);
-    setLikedPhotos(updatedLikedPhotos);
-  };
-  
 
   return (
     <div className="home-route">
@@ -29,8 +16,8 @@ const HomeRoute = ({openModal}) => {
       <PhotoList
         photos={photos}
         likedPhotos={likedPhotos}
-        onLikePhoto={handleLikePhoto}
-        onUnlikePhoto={handleUnlikePhoto}
+        onLikePhoto={onLikePhoto}
+        onUnlikePhoto={onUnlikePhoto}
         openModal={openModal}
       />
     </div>
