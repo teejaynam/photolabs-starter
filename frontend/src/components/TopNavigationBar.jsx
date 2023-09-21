b22d1115cd90c676bd4ca423772b27404f6a70af
TJ <teejaynam@gmail.com>
2023-09-21 12:16:28 -0400

    add TopicList container and FavBadge with appropriate data passed as props

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-
 import '../styles/TopNavigationBar.scss'
+import TopicList from './TopicList';
+import FavBadge from './FavBadge';
+import topics from 'mocks/topics';
 
-const TopNavigation = () => {
+const TopNavigation = ({ isFavPhotoExist }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <TopicList topics={topics}/>
+      <FavBadge isFavPhotoExist={isFavPhotoExist}/>
     </div>
   )
 }
