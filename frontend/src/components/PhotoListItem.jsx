2778542d7461ea14815700469ce997fe0535a097
TJ <teejaynam@gmail.com>
2023-09-21 12:15:27 -0400

    remove sample data and add handleClick function that uses function passed as prop, also create HTML structure for the Item itself

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,42 @@
 import React from "react";
-
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+const PhotoListItem = (props) => {
+  const { username, imageSource, id, location, profile, isLiked, onLike, onUnlike, openModal } = props;
 
-const PhotoListItem = () => {
-  /* Insert React */
+  const handleClick = () => {
+    openModal(id);
+  }
+
+  return (
+    <div className="photo-list__item" onClick={handleClick}>
+      <PhotoFavButton
+        isLiked={isLiked}
+        onLike={onLike}
+        onUnlike={onUnlike}
+      />
+      <img src={imageSource} className="photo-list__image"/>
+      <div className="photo-list__user-details">
+        <img
+          src={profile}
+          alt={`${username}'s profile`}
+          className="photo-list__user-profile"
+        />
+        <div className="photo-list__user-info">
+          <div>
+            <span className="">{username}</span>
+          </div>
+          <div>
+            <span className="photo-list__user-location">
+              {location.city}, {location.country}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PhotoListItem;
