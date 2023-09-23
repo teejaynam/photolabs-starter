7adc1186dac8242984f371bbf9de5d9f0e31d13e
TJ <teejaynam@gmail.com>
2023-09-23 15:50:25 -0400

    update navigation bar to use data passed as props instead of directly importing mock data

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
-import topics from 'mocks/topics';
 
-const TopNavigation = ({ isFavPhotoExist }) => {
+const TopNavigation = ({ isFavPhotoExist, topics }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
