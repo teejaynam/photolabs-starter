5c4351de09a660d6b23f42c236a3c138630ec35f
TJ <teejaynam@gmail.com>
2023-09-23 21:44:59 -0400

    update TopNavigationBar to pass fetchphotosbytopic function to topiclist

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,11 +3,11 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
-const TopNavigation = ({ isFavPhotoExist, topics }) => {
+const TopNavigation = ({ isFavPhotoExist, topics, fetchPhotosByTopic }) => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={topics}/>
+      <TopicList topics={topics} fetchPhotosByTopic={fetchPhotosByTopic}/>
       <FavBadge isFavPhotoExist={isFavPhotoExist}/>
     </div>
   )
