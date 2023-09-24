8a8887f51e0aec2b40a107691af9a8da6eb9c5a2
TJ <teejaynam@gmail.com>
2023-09-23 21:43:41 -0400

    update component to pass fetchPhotosBytopic function to topiclistitem

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -4,6 +4,7 @@ import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
 const TopicList = (props) => {
+  const {fetchPhotosByTopic} = props;
   return (
     <div className="top-nav-bar__topic-list">
       {props.topics.map((topic) => (
@@ -12,6 +13,7 @@ const TopicList = (props) => {
           id={topic.id}
           slug={topic.slug}
           title={topic.title}
+          fetchPhotosByTopic={fetchPhotosByTopic}
         />
       ))}
     </div>
