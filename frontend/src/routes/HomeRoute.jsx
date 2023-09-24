fe13c0047a5778c58f9b8b3f520904f87ab1c8fa
TJ <teejaynam@gmail.com>
2023-09-23 21:45:29 -0400

    update homeroute to use fetchPhotosByTopic function and pass it into TopNavigationBar

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -5,13 +5,13 @@ import TopNavigationBar from 'components/TopNavigationBar';
 import useApplicationData from 'hooks/useApplicationData';
 
 const HomeRoute = (props) => {
-  const {openModal, likedPhotos, onLikePhoto, onUnlikePhoto, photos, topics} = props;
+  const {openModal, likedPhotos, onLikePhoto, onUnlikePhoto, photos, topics, fetchPhotosByTopic} = props;
 
   const isFavPhotoExist = likedPhotos.length > 0;
 
   return (
     <div className="home-route">
-      <TopNavigationBar isFavPhotoExist={isFavPhotoExist} topics={topics}/>
+      <TopNavigationBar isFavPhotoExist={isFavPhotoExist} topics={topics} fetchPhotosByTopic={fetchPhotosByTopic}/>
       <PhotoList
         photos={photos}
         likedPhotos={likedPhotos}
