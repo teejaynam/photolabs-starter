afd3b95fdb44b0d63d9557c629df1cf014b85d8d
TJ <teejaynam@gmail.com>
2023-09-23 21:44:26 -0400

    update topiclistitem to include click handler function to invoke fetchPhotosBytopic function onclick

--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -4,9 +4,15 @@ import "../styles/TopicListItem.scss";
 
 
 const TopicListItem = (props) => {
+  const {id, title, fetchPhotosByTopic} = props;
+
+  const handleTopClick = () => {
+    fetchPhotosByTopic(id);
+  }
+
   return (
-    <div className="topic-list__item">
-        <span className="topic-list__item">{props.title}</span>
+    <div className="topic-list__item" onClick={handleTopClick}>
+        <span className="topic-list__item">{title}</span>
     </div>
   );
 };
