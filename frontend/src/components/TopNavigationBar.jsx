7edc4befdc127823c1d1cb86cfe3b52f7553fd06
TJ <teejaynam@gmail.com>
2023-09-29 17:07:37 -0400

    add logoclick handler func to invoke passed prop funcs

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -3,12 +3,16 @@ import '../styles/TopNavigationBar.scss'
 import TopicList from './TopicList';
 import FavBadge from './FavBadge';
 
-const TopNavigation = ({ isFavPhotoExist, topics, fetchPhotosByTopic }) => {
+const TopNavigation = ({ isFavPhotoExist, topics, fetchPhotosByTopic, toggleShowLikedPhotos, handleLogoClick }) => {
+  const logoClick = () => {
+    handleLogoClick();
+  }
+
   return (
     <div className="top-nav-bar">
-      <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span className="top-nav-bar__logo" onClick={logoClick}>PhotoLabs</span>
       <TopicList topics={topics} fetchPhotosByTopic={fetchPhotosByTopic}/>
-      <FavBadge isFavPhotoExist={isFavPhotoExist}/>
+      <FavBadge isFavPhotoExist={isFavPhotoExist} toggleShowLikedPhotos={toggleShowLikedPhotos}/>
     </div>
   )
 }
