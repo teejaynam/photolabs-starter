f9b08b6cb567220b99b948e2d47fefaf40924903
TJ <teejaynam@gmail.com>
2023-09-29 17:08:01 -0400

    add functions to handle showLikedphotos state

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/HomeRoute.scss';
 import PhotoList from 'components/PhotoList';
 import TopNavigationBar from 'components/TopNavigationBar';
@@ -9,11 +9,31 @@ const HomeRoute = (props) => {
 
   const isFavPhotoExist = likedPhotos.length > 0;
 
+  const [showLikedPhotos, setShowLikedPhotos] = useState(false);
+
+  const toggleShowLikedPhotos = () => {
+    setShowLikedPhotos(!showLikedPhotos);
+  }
+
+  const filteredPhotos = showLikedPhotos
+  ? photos.filter((photo) => likedPhotos.includes(photo.id))
+  : photos;
+
+  const handleLogoClick = () => {
+    setShowLikedPhotos(false);
+  }
+
   return (
     <div className="home-route">
-      <TopNavigationBar isFavPhotoExist={isFavPhotoExist} topics={topics} fetchPhotosByTopic={fetchPhotosByTopic}/>
+      <TopNavigationBar 
+        isFavPhotoExist={isFavPhotoExist} 
+        topics={topics} 
+        fetchPhotosByTopic={fetchPhotosByTopic} 
+        toggleShowLikedPhotos={toggleShowLikedPhotos}
+        handleLogoClick={handleLogoClick}
+      />
       <PhotoList
-        photos={photos}
+        photos={filteredPhotos}
         likedPhotos={likedPhotos}
         onLikePhoto={onLikePhoto}
         onUnlikePhoto={onUnlikePhoto}
